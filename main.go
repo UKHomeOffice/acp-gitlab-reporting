3f7132f0ecb692b38da7fb4abeb6a346e5af9655
Chris Freeman <chris.freeman@digital.homeoffice.gov.uk>
2023-02-17 14:04:30 +0000

    seperated gitlab api call and extraction

--- main.go
+++ main.go
@@ -18,30 +18,41 @@ var (
 	reporting_access_token = flag.String("reporting_access_token", "", "")
 )
 
-func main() {
-	flag.Parse()
-
+func getGitlabStatistics() []byte {
 	req, _ := http.NewRequest("GET", *gitlab_url+"/api/v4/application/statistics", nil)
 	req.Header.Add("PRIVATE-TOKEN", *gitlab_access_token)
 
+	response_body, _ := http_request(req)
+	return response_body
+}
+
+func extractGitlabStatistics(response []byte) (int64, int64) {
 	var application_statistics map[string]string
-	if err := json.Unmarshal(http_request(req), &application_statistics); err != nil {
+	if err := json.Unmarshal(response, &application_statistics); err != nil {
 		panic(err)
 	}
 
-	total, err := strconv.ParseInt(strings.Replace(application_statistics["projects"], ",", "", -1), 10, 32)
+	total, err := strconv.ParseInt(strings.Replace(application_statistics["projects"], ",", "", -1), 10, 64)
 	if err != nil {
 		panic(err)
 	}
 
-	forks, err := strconv.ParseInt(strings.Replace(application_statistics["forks"], ",", "", -1), 10, 32)
+	forks, err := strconv.ParseInt(strings.Replace(application_statistics["forks"], ",", "", -1), 10, 64)
 	if err != nil {
 		panic(err)
 	}
+	return total, forks
+}
+
+func main() {
+	flag.Parse()
+
+	gitlab_statistics_repsonse := getGitlabStatistics()
+	total, forks := extractGitlabStatistics(gitlab_statistics_repsonse)
 
 	report_json, err := json.Marshal(report_payload{
-		Total:    int(total),
-		Forks:    int(forks),
+		Total:    total,
+		Forks:    forks,
 		Archived: 0,
 		Personal: 0,
 		Groups:   0,
@@ -55,7 +66,7 @@ func main() {
 
 	reporting_req, _ := http.NewRequest("PUT", *reporting_url, bytes.NewBuffer(report_json))
 	reporting_req.Header.Add("x-api-key", *reporting_access_token)
-	body := http_request(reporting_req)
+	body, _ := http_request(reporting_req)
 	fmt.Println(body)
 
 }
@@ -64,14 +75,14 @@ type application_statistics struct {
 }
 
 type report_payload struct {
-	Total    int `json:"total"`    // The total number of repos
-	Forks    int `json:"forks"`    // The number of forks
-	Archived int `json:"archived"` // The number of archived/unused repos if possible
-	Personal int `json:"personal"` // The number of personal user repos
-	Groups   int `json:"groups"`   // The number of GitLab groups repos
+	Total    int64 `json:"total"`    // The total number of repos
+	Forks    int64 `json:"forks"`    // The number of forks
+	Archived int64 `json:"archived"` // The number of archived/unused repos if possible
+	Personal int64 `json:"personal"` // The number of personal user repos
+	Groups   int64 `json:"groups"`   // The number of GitLab groups repos
 }
 
-func http_request(req *http.Request) []byte {
+func http_request(req *http.Request) ([]byte, http.Header) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -88,5 +99,5 @@ func http_request(req *http.Request) []byte {
 	if err != nil {
 		panic(err)
 	}
-	return body
+	return body, resp.Header
 }
